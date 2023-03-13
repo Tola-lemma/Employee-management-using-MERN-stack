78f631737e803996c5d66780f28a4ec487d78096
Tola-lemma <tolalemma@gmail.com>
2023-03-13 11:35:49 +0300

    connection reqiure and Api call

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
-
+const bodyparser = require('body-parser');
+const client = require('./server/connection')
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended:true}))
+
+
 app.get('/', (req, res) => {
-    res.send("Welcome to Employe management! ");
+  res.send("Welcome to Employe management! ");
 })
+app.use('/api/department',require('./Controller/controller'))
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT} ...`));
+
