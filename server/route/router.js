60e1a392f6085db1cedfb5e41c21376e2c90bcd9
Tola-lemma <tolalemma@gmail.com>
2023-03-14 00:24:25 +0300

    route to post method

--- server/route/router.js
+++ server/route/router.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../Controller/controller')
 router.get('/api/department',controller.getAllDepartment)
-
+router.post('/api/department',controller.createDepartment)
 
 module.exports = router
