0ddc2cdc138fa869a7a2b7cdab6759a0b1d06d5c
Tola-lemma <tolalemma@gmail.com>
2023-03-14 00:49:00 +0300

    route to put method is specified

--- server/route/router.js
+++ server/route/router.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const controller = require('../Controller/controller')
 router.get('/api/department',controller.getAllDepartment)
 router.post('/api/department',controller.createDepartment)
-
+router.put('/api/department/:id',controller.UpdateDepartment)
 module.exports = router
