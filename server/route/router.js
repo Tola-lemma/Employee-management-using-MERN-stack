a8033ebdde80a2600f59a62daad544d672d26104
Tola-lemma <tolalemma@gmail.com>
2023-03-14 11:52:25 +0300

    delete route  is added

--- server/route/router.js
+++ server/route/router.js
@@ -4,4 +4,5 @@ const controller = require('../Controller/controller')
 router.get('/api/department',controller.getAllDepartment)
 router.post('/api/department',controller.createDepartment)
 router.put('/api/department/:id',controller.UpdateDepartment)
+router.delete('/api/department/:id',controller.deleteDepartment)
 module.exports = router
