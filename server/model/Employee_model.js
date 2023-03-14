607733c3eaea0fe8d319284681a57f8d7c802915
Tola-lemma <tolalemma@gmail.com>
2023-03-14 17:11:35 +0300

    employee model is created

--- server/model/Employee_model.js
+++ server/model/Employee_model.js
@@ -10,7 +10,7 @@ EmployeeId: {
     type: String,
     required: true
   },
-  Deparment: {
+  Department: {
     type: String,
     required: true
   },
@@ -19,7 +19,8 @@ EmployeeId: {
     required: true
   },
   PhotoFileName: {
-    type: Buffer
+    type: String,
+    required:true
   }
 },{collection:'Employee'});
 const employedb = mongoose.model('employedb',employee)
