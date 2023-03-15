f239db98ed9d1237c4ed71ff580fd2910a3a4132
Tola-lemma <tolalemma@gmail.com>
2023-03-15 11:08:30 +0300

    Modified unnecessary space removed

--- Backend/index.js
+++ Backend/index.js
@@ -6,18 +6,14 @@ const connectDB = require('./server/database/connection');
 var fileUpload = require('express-fileupload')
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 app.use(fileUpload());
 app.use(cors());
 dotenv.config({path:'config.env'})
 app.use(morgan('tiny'));
 connectDB();
-
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended:true}))
-
-
 app.get('/', (req, res) => {
   res.send("Welcome to Employe management! ");
 })
