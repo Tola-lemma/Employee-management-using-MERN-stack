bbf0cb132bbde8ecc29d1729401468b98ff6a8ad
Tola-lemma <tolalemma@gmail.com>
2023-03-16 14:36:50 +0300

    URL for Employee and Profile Photo is added

--- frontend/src/API_URL/api_url.js
+++ frontend/src/API_URL/api_url.js
@@ -1,3 +1,5 @@
 export const API_URL={
     DEPARTMENT:'http://localhost:3000/api/department/',
+    EMPLOYEE:'http://localhost:3000/api/employee/',
+    PROFILEPHOTO:'http://localhost:3000/api/employee/fileupload/'
 }
