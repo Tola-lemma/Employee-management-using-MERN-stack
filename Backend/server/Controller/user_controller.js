4e7defd9edcac25c928eb8046271555549dc63cc
Tola-lemma <tolalemma@gmail.com>
2023-03-20 22:40:29 +0300

    Modified is jwt

--- Backend/server/Controller/user_controller.js
+++ Backend/server/Controller/user_controller.js
@@ -1,32 +1,53 @@
 const Userdb = require("../model/user");
-
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+const saltRounds = 10;
 exports.CreateUser = (req,res)=>{
   if(!req.body){
     res.status(404).send({message:"Fields cannot be Empty!"});
     return
   }
-  const newUser = new Userdb({
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+   const newUser = new Userdb({
     email:req.body.email,
-    password:req.body.password
+    password:hash
   })
   newUser.save()
        .then((data)=>{
-        res.status(201).json(data)
+        jwt.sign(
+        {id:data.id},
+        process.env.JWT_SECRET,
+        {expiresIn:'24h'},
+        (err,token)=>{
+            if(err) throw err;
+             res.status(201)
+                 .json({token,data})
+        }
+        )
        })
        .catch((err)=>{
-        res.status(500).json({ message: 'while creating User' });
+        res.status(500).json({ message: 'Error while creating User' });
     });
+});
 }
 exports.UserLogin = async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+    const {email, password} = req.body;
     try {
       const foundUser = await Userdb.findOne({ email: email });
-      if (foundUser && foundUser.password === password) {
-        res.status(200).send('Successfully login!');
-      } else {
-        res.status(401).send('Invalid email or password.');
-      }
+      const isMatch = await bcrypt.compare(password,foundUser.password);
+      if(isMatch) {
+        jwt.sign(
+            {id:foundUser.id},
+            process.env.JWT_SECRET,
+            {expiresIn:'24h'},
+            (err,token)=>{
+                if(err) throw err;
+                 res.status(200)
+                     .json({token,foundUser})
+                    })
+      }else{
+    res.status(401).send('Invalid credentials.');
+    }   
     } catch (error) {
       console.log(error);
       res.status(500).send('Internal server error.');
--- Backend/server/route/router.js
+++ Backend/server/route/router.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Authent = require('../middleware/auth')
 //department
 const departmentController = require('../Controller/department_controller')
 router.get('/api/department',departmentController.getAllDepartment)
-router.post('/api/department',departmentController.createDepartment)
-router.put('/api/department/:id',departmentController.UpdateDepartment)
-router.delete('/api/department/:id',departmentController.deleteDepartment)
+router.post('/api/department',Authent,departmentController.createDepartment)
+router.put('/api/department/:id',Authent,departmentController.UpdateDepartment)
+router.delete('/api/department/:id',Authent,departmentController.deleteDepartment)
 //employee
 const employeeController = require('../Controller/Employee_controller')
 router.get('/api/employee',employeeController.getAllEmployee)
-router.post('/api/employee',employeeController.createEmployee)
-router.put('/api/employee/:id',employeeController.UpdateEmployee)
-router.delete('/api/employee/:id',employeeController.deleteEmployee)
+router.post('/api/employee',Authent,employeeController.createEmployee)
+router.put('/api/employee/:id',Authent,employeeController.UpdateEmployee)
+router.delete('/api/employee/:id',Authent,employeeController.deleteEmployee)
 //fileuploads
 const fileUploading = require('../Controller/fileupload')
 router.post('/api/fileupload',fileUploading.EmployeeProfile)
 //user
-const user = require('../Controller/user_controller')
+const user = require('../Controller/user_controller');
+const Auth = require('../middleware/auth');
+const auth = require('../middleware/auth');
 router.post('/api/register',user.CreateUser)
 router.post('/api/login',user.UserLogin);
 module.exports = router
