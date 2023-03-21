6691951db8965d327037b1234c932d9e45be72bb
Tola-lemma <tolalemma@gmail.com>
2023-03-21 10:23:39 +0300

    API URL of login and register is added

--- frontend/src/API_URL/api_url.js
+++ frontend/src/API_URL/api_url.js
@@ -2,5 +2,10 @@ export const API_URL={
     DEPARTMENT:'http://localhost:3000/api/department/',
     EMPLOYEE:'http://localhost:3000/api/employee/',
     PROFILEPHOTO:'http://localhost:3000/api/fileupload/',
-    photosPath :`http://localhost:3000/profile/`
+    photosPath :`http://localhost:3000/profile/`,
+
+
+    REGISTER:'http://localhost:3000/api/register/',
+    LOGIN:'http://localhost:3000/api/login/',
+    
 }
