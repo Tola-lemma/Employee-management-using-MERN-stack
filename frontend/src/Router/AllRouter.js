a9366afa686393e7f84d9bfd09e0047e56c4a9a3
Tola-lemma <tolalemma@gmail.com>
2023-03-22 00:13:47 +0300

    Modified with protected route

--- frontend/src/Router/AllRouter.js
+++ frontend/src/Router/AllRouter.js
@@ -1,22 +1,23 @@
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-// import { DepartmentPage } from '../Component/Department'
+import {createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { DepartmentPage } from '../Component/Department'
 import { EmpoyeePage } from '../Component/Employee'
 import { HomePage } from '../Component/Home'
-import { ProtectedDepartmentPage } from './protectedRoute/protected'
-export const AllRouter =()=>{
-const router = createBrowserRouter([
-    {
-        path:'/',
-        element:<HomePage />
-    },
-    {
-        path:'/department',
-        element:<ProtectedDepartmentPage />
-    },
-    {
-        path:'/employee',
-        element:<EmpoyeePage />
-    },
-])
-return <RouterProvider router={router}/>
-} 
+export const AllRouter =()=> {
+    const router = createBrowserRouter([
+        {
+            path:'/',
+            element:<HomePage />
+        },
+        {
+            path:'/department',
+            element:
+            localStorage.getItem('token') && <DepartmentPage />
+        },
+        {
+            path:'/employee',
+            element:
+            localStorage.getItem('token')&& <EmpoyeePage />
+        },
+      ])
+      return  <RouterProvider router={router} />
+      }
