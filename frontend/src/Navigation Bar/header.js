5f63dc277d923ad31e1f3466e49f4ba58d50911a
Tola-lemma <tolalemma@gmail.com>
2023-03-22 00:18:36 +0300

    Logout is handled

--- frontend/src/Navigation Bar/header.js
+++ frontend/src/Navigation Bar/header.js
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom"
 import  '.././App.css'
 export const NavBar = () => {
-  
+  const handleLout = ()=>{
+    localStorage.removeItem("token");
+  }
 return (
     <div className="navbarCustom">
-        <h1 style={{textAlign:"center"}}>Employee Management System</h1>
-       <nav className="navbar  navbar-expand-sm navbar-dark bg-light text-uppercase ">
+      <button className="Logout" onClick={handleLout}><Link className="btn btn-light btn-outline-primary LogoutLink" to="/">logout <span><i className="fa-sharp fa-solid fa-right-from-bracket"  style={{ fontSize: "17px" }}></i></span></Link></button>
+       <nav className="navbar  navbar-expand-sm navbar-dark bg-light text-uppercase " style={{backgroundImage:"radial-gradient( circle 654px at 0.6% 48%,  rgba(12,170,255,1) 0%, rgba(151,255,129,1) 99.3% )"}}>
         <div className="container-fluid " >
             <ul className="navbar-nav ms-5 mt-2">
               <li className="nav-item">
-                <Link className="btn btn-light btn-outline-primary" to="/">Home</Link>
+                
               </li>
               <li className="nav-item">
                 <Link className="btn btn-light btn-outline-primary" to="/department">Department</Link>
@@ -20,6 +22,7 @@ return (
             </ul>
           </div>
       </nav>
+        <h1 style={{textAlign:"center"}}>Employee Management System</h1>
     </div>
 )
 }
