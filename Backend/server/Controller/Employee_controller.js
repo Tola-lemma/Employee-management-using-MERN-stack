3e1874e09dc6c86bfbd014ccdfbc1350146e6260
Tola-lemma <tolalemma@gmail.com>
2023-03-22 10:08:35 +0300

    .then is changed to async/await

--- Backend/server/Controller/Employee_controller.js
+++ Backend/server/Controller/Employee_controller.js
@@ -1,67 +1,64 @@
 const employedb = require('../model/Employee_model')
 const uuid = require('uuid')
-exports.getAllEmployee=(req,res)=>{
-    employedb.find()
-    .then(employee=>{
+exports.getAllEmployee= async (req,res)=>{
+  try{  
+  const employee = await employedb.find()
      res.json(employee);
-    })
-    .catch(err=>{
+  }catch(err){
      res.status(500).send({message:err.message||`Error while retrieving Employee information`})
-    });
+    }
  }
- exports.createEmployee = (req, res) => {
-  if (!req.body) {
-    return res.status(400).json({ message: 'Content cannot be empty!' });
-  }
-
+ exports.createEmployee = async (req, res) => {
+   const {EmployeeName,Department,Date_of_Joining,PhotoFileName} = req.body
+   if (!EmployeeName&& !Department && !Date_of_Joining && !PhotoFileName) {
+     return res.status(400).json({ message: 'Content cannot be empty!' });
+   }
+try{
   const employee = new employedb({
     EmployeeId: uuid.v4().slice(0, 4),
-    EmployeeName: req.body.EmployeeName,
-    Department: req.body.Department,
-    Date_of_Joining: req.body.Date_of_Joining,
-    PhotoFileName: req.body.PhotoFileName,
+    EmployeeName,
+    Department,
+    Date_of_Joining,
+    PhotoFileName,
   });
 
-  employee.save()
-    .then((data) => {
+  const data = await employee.save()
       res.status(201).json(data);
-    })
-    .catch((err) => {
+
+  }catch(err)  {
       res.status(500).json({ message: 'Error adding Employee to database' });
-    });
+    }
 }
 
-exports.UpdateEmployee = (req,res)=>{
-    if(!req.body){
+exports.UpdateEmployee = async (req,res)=>{
+  if(!req.body){
       return res
            .status(404)
            .send({message:err.message ||`Error while updating the data cannot be empty`});
    }
+  try{  
     const id =req.params.id;
-  employedb.findByIdAndUpdate(id,req.body)
-       .then(data=>{
+  const data = await employedb.findByIdAndUpdate(id,req.body)
     if(data){
       res.json(data);
     } else{
       res.status(404).send({message:err.message ||`cannot update Employee with identified id ${id} or maybe user not found!`});
     }
-   })
-   .catch(err =>{
+  }catch(err){
   res.status(500).send({message: err.message || `error updating Employee information`});
-   })
+   }
   }
   
-  exports.deleteEmployee = (req,res)=>{
+  exports.deleteEmployee = async (req,res)=>{
+  try{  
     const id = req.params.id
-    employedb.findByIdAndDelete(id)
-    .then(data=>{
+   const data = await employedb.findByIdAndDelete(id)
       if(!data){
           res.status(404).send({message:`Cannot delete Employee with id ${id} maybe id is wrong!`});
       }else{
           res.json({message:`Employee was deleted successfully!`});
       }
-     })
-     .catch(err=>{
+    }catch(err){
       res.status(500).send({message:`Could not delete Employee with id ${id}`});
-     })
+     }
   }
--- Backend/server/Controller/department_controller.js
+++ Backend/server/Controller/department_controller.js
@@ -1,63 +1,63 @@
 const Departdb = require('../model/Deparment_model')
 const uuid = require('uuid')
-exports.getAllDepartment=(req,res)=>{
-  Departdb.find()
-   .then(departement=>{
+exports.getAllDepartment=async(req,res)=>{
+  try{
+ const departement = await Departdb.find()
     res.json(departement);
-   })
-   .catch(err=>{
+  }
+  catch(err){
     res.status(500).send({message:err.message||`Error while retrieving department information`})
-   });
+   };
 }
-exports.createDepartment = (req,res)=>{
+exports.createDepartment = async (req,res)=>{
     if(!req.body){
       res.status(400).send({message:'Content cannot be empty!'});
       return
   }
+  try{
   const department = new Departdb({
         DepartmentId: uuid.v4().slice(0,4),
         DepartmentName:req.body.DepartmentName
       })
-  department.save(department)
-            .then(data=>{
+  const data = await department.save()
             res.status(201).json(data);
-           })
-        .catch(err =>{
+    }   
+        catch(err){
         res.status(500).send('Error adding department to database');
-        })
+        }
 }
 
-exports.UpdateDepartment = (req,res)=>{
+exports.UpdateDepartment = async (req,res)=>{
   if(!req.body){
     return res
          .status(404)
          .send({message:err.message ||`Error while updating the data cannot be empty`});
  }
+ try{
   const id =req.params.id;
-Departdb.findByIdAndUpdate(id,req.body)
-     .then(data=>{
+const data = await Departdb.findByIdAndUpdate(id,req.body)
   if(!data){
       res.status(404).send({message:err.message ||`cannot update Department with identified id ${id} or maybe user not found!`});
   } else{
       res.json(data);
   }
- })
- .catch(err =>{
+}
+catch(err){
 res.status(500).send({message: err.message || `error updating Department information`});
- })
+ }
 }
 
-exports.deleteDepartment = (req,res)=>{
+exports.deleteDepartment = async (req,res)=>{
+  try{
   const id = req.params.id
-  Departdb.findByIdAndDelete(id)
-  .then(data=>{
+ const data= await Departdb.findByIdAndDelete(id)
     if(!data){
         res.status(404).send({message:`Cannot delete Department with id ${id} maybe id is wrong!`});
     }else{
         res.json({message:`Department was deleted successfully!`});
     }
-   })
-   .catch(err=>{
+  }
+   catch(err){
     res.status(500).send({message:`Could not delete Department with id ${id}`});
-   })
+   }
 }
